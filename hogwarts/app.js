ceee2d083b7f4de43e4b5e2a270c9221d929a9b7
Haleh Bozorgnia <bozorgnia2010@gmail.com>
2023-05-16 12:50:23 -0700

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -1,12 +1,21 @@
 $(() => {
-    const $container = $("#container")
-    console.log($container)
-    const $h1 = $("<h1>")
-    console.log($h1)
-    $h1.text("Hogwarts")
-    $container.append($h1)
+  const $container = $("#container");
+  console.log($container);
+  const $h1 = $("<h1>");
+  console.log($h1);
+  $h1.text("Hogwarts");
+  $container.append($h1);
 
+  //Year 2
+  const $h2 = $("<h2>");
+  $container.append($h2.text("Haleh Bozorgnia"));
+  console.log($h2);
 
+  const $h3 = $ ("<h3>")
+  $container.append(($h3.text("Hufflepuff")))
 
-
+  const $h4 = $ ("<h4>")
+  $h4.addClass("cat")
+  $container.append($h4.text("pishi"))
+console.log($container)
 });
