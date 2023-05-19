3beee3594c23eae64e5983e4fe48431985862c15
Haleh Bozorgnia <bozorgnia2010@gmail.com>
2023-05-18 22:15:21 -0700

    Commit 6 - A cauliflower is a cabbage with a college education - Mark Twain

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -18,6 +18,8 @@ $(() => {
   $h4.addClass("cat");
   $container.append($h4.text("pishi"));
   console.log($container);
+  const $h = $("<h4>");
+  $container.append($h.text("Holly Wand with Unicorn Hair Core"));
 
   //Year 3
   const $ul = $("<ul>");
@@ -74,9 +76,42 @@ $(() => {
   const $tr4 = $("<tr>");
   const $td7 = $("<td>").text("Wednesday");
   const $td8 = $("<td>").text("Transfiguration");
-  $tr3.append($td7, $td8);
+  $tr4.append($td7, $td8);
   //append roes to table
   $table.append($tr1, $tr2, $tr3, $tr4);
   //append table to container
   $container.append($table);
+
+  // Year 5
+  //Break your wand!
+  $h.remove();
+
+  //Class was hard! Drink all your butter beer! (remove just the butter beer from your list)
+  $li1.remove();
+  //Get a new wand (add the same element back with new text describing your new wand. Be sure to insert it after your pet in the DOM)
+
+  const $new_h = $("<h4>");
+  $new_h.text("new Holly Wand with Unicorn Hair Core");
+  $container.append($new_h);
+  $h4.after($new_h);
+  //Make your new wand stand out by adding a color of indigo (or whatever color you like). But do it with magic (jQuery): Don't add this css in your main.css file
+  $new_h.css("color", "indigo");
+  //Send your pet on a spy mission
+  $h4.detach();
+  //Have your pet come back (remove your pet from the DOM, put it back in its original location)
+  $h4.remove();
+  $h3.append($h4);
+  //Year 6
+  $(".secret").hide("slow");
+
+  //Nosey roommate falls asleep 2 seconds later
+  $(".secret").hide("2000");
+  // Use jQuery method show to reveal all of your belongings
+
+  $(".secret").show("slow");
+  //add the class cabbage to your pet's leash
+  $li5.addClass("cabbage");
+  //add an attribute of color:CHARTREUSE
+  //Fix your pet's leash by removing the class of cabbage
+  $li5.removeClass("cabbage");
 });
